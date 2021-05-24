0332922c0cffe558a367d4681d28522346f58959
MusaUnleashed <mosa-rko@windowslive.com>
2021-05-24 12:50:05 +0300

    render works

--- dist/main.js
+++ dist/main.js
@@ -1,33 +1,45 @@
 
-const renderer=new Render();
+const renderer = new Render();
 
 
-$('#getIngredent').on('click',function () { 
-    let input_element=$('#recipe-input');
-    let input_val=input_element.val();
+$('#getIngredent').on('click', function () {
+    let input_element = $('#recipe-input');
+    let input_val = input_element.val();
 
-   
+
 
     // $.get(`/recipes/${input_val}`, function (response) {
-        
+
 
     //     console.log(response);
     // })
     $.ajax({
         method: "GET",
         url: `/recipes/${input_val}`,
-         success: function (response) {
-            // let filterd_result=response.map(e => {e.ingredients,e.title,e.thumbnail,e.href});
+        success: function (response) {
 
-             console.log('filterd_result +++');
-             console.log(filterd_result )
-            //  let filterd_result=response.filter(r=>return{ {r.r}})
-
-             renderer.renderRecipes(response);
+            console.log('filterd_result +++');
+            console.log(response)
+            let new_arr =[]
+            //  response.forEach(recipe=>{ //chek later no time 
+            //     new_arr.push(filtterRecipes(recipe));
+            // })
+            console.log('res')
+            renderer.renderRecipes(response);
 
         }
     })
-    
+
+    // const filtterRecipes = function (recipe) {
+    //     let new_Recipe = {};
+    //     new_Recipe[ingredients] = recipe[ingredients];
+    //     new_Recipe[title] = recipe[title];
+    //     new_Recipe[thumbnail] = recipe[thumbnail];
+    //     new_Recipe[href] = recipe[href];
+    //     return new_Recipe
+
+    // }
+
 
 
 
